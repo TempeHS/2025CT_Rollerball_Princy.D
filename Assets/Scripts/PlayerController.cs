0c4ee76486410281b4c2fdea3e6b01f3470271e9
Princy D <princy.dumais@education.nsw.gov.au>
2025-03-13 14:35:16 +1100

    Revert "made an enemy"
    This reverts commit a9a0a12a421ae0fc7f595228eea9ca40707ba7c0.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,7 +17,6 @@ public class PlayerController : MonoBehaviour
  public float speed = 0; 
 
  public TextMeshProUGUI countText; 
- public GameObject winTextObject;
 
  // Start is called before the first frame update.
  void Start()
@@ -27,7 +26,6 @@ public class PlayerController : MonoBehaviour
         count = 0; // Set the count as 0.
 
         SetCountText();
-        winTextObject.SetActive(false);
     }
  
  // This function is called when a move input is detected.
@@ -44,10 +42,6 @@ public class PlayerController : MonoBehaviour
     void SetCountText() 
    {
        countText.text =  "Score 12/" + count.ToString();
-       if(count >= 12)
-       {
-         winTextObject.SetActive(true);
-       }
    }
 private void OnCollisionEnter(Collision collision)
 {
